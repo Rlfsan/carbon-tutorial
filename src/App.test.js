3050ea259d1cfe9c2d5aa5408fbb8d1af3e7a2b7
Rlfsan <rlfsan@br.ibm.com>
2020-02-19 10:30:07 -0300

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './components/TutorialHeader/_tutorial-header.scss';
+import './content/LandingPage/_landing-page.scss';
+import './content/RepoPage/_repo-page.scss';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
